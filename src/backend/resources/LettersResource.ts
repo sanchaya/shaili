7e6fbcfeab71ef56df55c7807fd4e442a3b3e894
Sahayaraj <kulanthaisahayaraj@kuruntham.com>
2024-01-09 14:00:55 +0530

    letter import change

--- src/backend/resources/LettersResource.ts
+++ src/backend/resources/LettersResource.ts
@@ -4,8 +4,8 @@ import { Components, componentLoader } from "../../frontend/components.js";
 import { menu } from "../../common/menu.js";
 import { ActionContext, ActionRequest } from "adminjs";
 import {
-  LetterDeleteBefore,
-  LetterDeleteHandler,
+    LetterDeleteBefore,
+    LetterDeleteHandler,
 } from "../utils/LetterResourceUtils.js";
 import csvParser from "csv-parser";
 import fs from "fs";
@@ -13,153 +13,163 @@ import { LetterTypes } from "../db/models/LetterTypes.js";
 import { Languages } from "../db/models/Languages.js";
 
 const isAccessible = (context: ActionContext, role: number) => {
-  const { currentAdmin } = context;
-  return role === currentAdmin?.role;
+    const { currentAdmin } = context;
+    return role === currentAdmin?.role;
 };
 
 const getLetterTypeId = async (lettertype, language, currentAdmin) => {
-  const letterType = await LetterTypes.findOne({
-    where: { type: lettertype, language: language },
-  });
-
-  if (letterType) {
-    return letterType?.dataValues.id;
-  } else {
-
-    const language_record = await Languages.findOne({
-      where: { language_code: language },
+    const letterType = await LetterTypes.findOne({
+        where: { type: lettertype, language: language },
     });
 
-    if (language_record) {
-      const newLetterType = await LetterTypes.create({
-        type: lettertype,
-        language: language,
-        created_by: currentAdmin.id,
-        updated_by: currentAdmin.id,
-      });
+    if (letterType) {
+        return letterType?.dataValues.id;
+    } else {
+        const language_record = await Languages.findOne({
+            where: { language_code: language },
+        });
 
-      return newLetterType.dataValues.id;
+        if (language_record) {
+            const newLetterType = await LetterTypes.create({
+                type: lettertype,
+                language: language,
+                created_by: currentAdmin.id,
+                updated_by: currentAdmin.id,
+            });
+
+            return newLetterType.dataValues.id;
+        }
+        return null;
     }
-    return null;
-  }
 };
 
 const importBefore = async (request: ActionRequest, context: ActionContext) => {
-  const filePath = request.payload?.file.path;
-  const result: Letters[] = [];
-  const { currentAdmin } = context;
+    const filePath = request.payload?.file.path;
+    const result: Letters[] = [];
+    const { currentAdmin } = context;
 
-  const getLetterTypeIdAsync = async (letterType: string, language: string) => {
-    return await getLetterTypeId(letterType, language, currentAdmin);
-  };
+    const getLetterTypeIdAsync = async (
+        letterType: string,
+        language: string
+    ) => {
+        return await getLetterTypeId(letterType, language, currentAdmin);
+    };
 
-  const parser = fs.createReadStream(filePath).pipe(csvParser());
+    const parser = fs.createReadStream(filePath).pipe(csvParser());
 
-  for await (const data of parser) {
-    if (data.letter && data.letter_type) {
-      const letter = await Letters.findOne({
-        where: { letter: data.letter },
-      });
+    for await (const data of parser) {
+        data.letter = data.letter.replace(/\s+/g, " ").trim();
+        data.letter_type = data.letter_type.replace(/\s+/g, " ").trim();
+        data.language = data.language.toLowerCase().replace(/\s+/g, " ").trim();
+        if (data.letter && data.letter_type) {
+            const letter = await Letters.findOne({
+                where: { letter: data.letter },
+            });
 
-      if (!letter) {
-        data.language = data.language.toLowerCase();
+            if (!letter) {
+                const letterType = await getLetterTypeIdAsync(
+                    data.letter_type,
+                    data.language
+                );
 
-        const letterType = await getLetterTypeIdAsync(
-          data.letter_type,
-          data.language
-        );
-
-        if (letterType !== null) {
-          data.letter_type = letterType;
-          data.created_by = currentAdmin?.id;
-          data.updated_by = currentAdmin?.id;
-          data.user_defined = 0;
-          result.push(data);
+                if (letterType !== null) {
+                    data.letter_type = letterType;
+                    data.created_by = currentAdmin?.id;
+                    data.updated_by = currentAdmin?.id;
+                    data.user_defined = 0;
+                    result.push(data);
+                }
+            }
         }
-      }
     }
-  }
 
-  return { request, context, result };
+    return { request, context, result };
 };
 
 const importHandler = async (props) => {
-  const records = await Letters.bulkCreate(props.result);
-  const { context } = props;
-  const { resource, h } = context;
-  const createdRecords = records.map((record) => record.dataValues.letter);
+    const records = await Letters.bulkCreate(props.result);
+    const { context } = props;
+    const { resource, h } = context;
+    const createdRecords = records.map((record) => record.dataValues.letter);
 
-  if (records) {
-    return {
-      redirectUrl: h.resourceUrl({
-        resourceId: resource._decorated?.id() || resource.id(),
-      }),
-      notice: {
-        message:
-          createdRecords.length == 1
-            ? createdRecords.length + " Letter added"
-            : createdRecords.length + " Letters added",
-        type: "success",
-      },
-      createdRecords: createdRecords.length,
-    };
-  }
+    if (records) {
+        return {
+            redirectUrl: h.resourceUrl({
+                resourceId: resource._decorated?.id() || resource.id(),
+            }),
+            notice: {
+                message:
+                    createdRecords.length == 1
+                        ? createdRecords.length + " Letter added"
+                        : createdRecords.length + " Letters added",
+                type: "success",
+            },
+            createdRecords: createdRecords.length,
+        };
+    }
 };
 
 export const LetterResource = {
-  resource: Letters,
-  options: {
-    navigation: menu.Letters,
-    listProperties: ["letter", "language", "letter_type", "user_defined"],
-    showProperties: [
-      "letter",
-      "language",
-      "letter_type",
-      "user_defined",
-      "created_by",
-      "updated_by",
+    resource: Letters,
+    options: {
+        navigation: menu.Letters,
+        listProperties: ["letter", "language", "letter_type", "user_defined"],
+        showProperties: [
+            "letter",
+            "language",
+            "letter_type",
+            "user_defined",
+            "created_by",
+            "updated_by",
+        ],
+        filterProperties: ["letter", "language", "letter_type", "user_defined"],
+        actions: {
+            bulkDelete: { isAccessible: false },
+            list: {
+                isAccessible: (context: ActionContext) =>
+                    isAccessible(context, 1),
+            },
+            edit: {
+                isAccessible: (context: ActionContext) =>
+                    isAccessible(context, 1),
+                component: Components.EditLetter,
+            },
+            show: {
+                isAccessible: (context: ActionContext) =>
+                    isAccessible(context, 1),
+            },
+            delete: {
+                isAccessible: (context: ActionContext) =>
+                    isAccessible(context, 1),
+                before: [LetterDeleteBefore],
+                handler: [LetterDeleteHandler],
+            },
+            new: {
+                isAccessible: (context: ActionContext) =>
+                    isAccessible(context, 1),
+                component: Components.AddLetter,
+            },
+            import: {
+                isAccessible: (context: ActionContext) =>
+                    isAccessible(context, 1),
+                before: [importBefore],
+                handler: [importHandler],
+                component: Components.ImportComponentNew,
+            },
+            export: {
+                isAccessible: (context: ActionContext) =>
+                    isAccessible(context, 1),
+            },
+        },
+        properties: {
+            language: {
+                reference: "languages",
+            },
+        },
+    },
+    features: [
+        importExportFeature({
+            componentLoader,
+        }),
     ],
-    filterProperties: ["letter", "language", "letter_type", "user_defined"],
-    actions: {
-      bulkDelete: { isAccessible: false },
-      list: {
-        isAccessible: (context: ActionContext) => isAccessible(context, 1),
-      },
-      edit: {
-        isAccessible: (context: ActionContext) => isAccessible(context, 1),
-        component: Components.EditLetter,
-      },
-      show: {
-        isAccessible: (context: ActionContext) => isAccessible(context, 1),
-      },
-      delete: {
-        isAccessible: (context: ActionContext) => isAccessible(context, 1),
-        before: [LetterDeleteBefore],
-        handler: [LetterDeleteHandler],
-      },
-      new: {
-        isAccessible: (context: ActionContext) => isAccessible(context, 1),
-        component: Components.AddLetter,
-      },
-      import: {
-        isAccessible: (context: ActionContext) => isAccessible(context, 1),
-        before: [importBefore],
-        handler: [importHandler],
-        component: Components.ImportComponentNew,
-      },
-      export: {
-        isAccessible: (context: ActionContext) => isAccessible(context, 1),
-      },
-    },
-    properties: {
-      language: {
-        reference: "languages",
-      },
-    },
-  },
-  features: [
-    importExportFeature({
-      componentLoader,
-    }),
-  ],
 };
